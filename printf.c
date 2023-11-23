709f1c06652a70c111cfa5836e279c5813269160
buy-digit <150363151+buy-digit@users.noreply.github.com>
2023-11-23 11:19:35 +0100

    Create printf.c

---
+++ printf.c
@@ -0,0 +1,51 @@
+#include "ft_printf.h"
+
+int	ft_params(char str, int i, va_list args)
+{
+	if (str == 's')
+		i += ft_putstr(va_arg(args, char *));
+	else if (str == 'd' || str == 'i')
+		i += ft_putnbr(va_arg(args, int));
+	else if (str == 'c')
+		i += ft_putchar(va_arg(args, int));
+	else if (str == 'x')
+		i += ft_lhex(va_arg(args, unsigned int));
+	else if (str == 'X')
+		i += ft_uhex(va_arg(args, unsigned int));
+	else if (str == 'u')
+		i += ft_unsigned(va_arg(args, unsigned int));
+	else if (str == 'p')
+	{
+		i += ft_putstr("0x");
+		i += ft_lhex(va_arg(args, unsigned long));
+	}
+	else if (str == '%')
+		i += ft_putchar(str);
+	else
+		i += ft_putchar(str);
+	return (i);
+}
+
+int	ft_printf(const char *str, ...)
+{
+	int		i;
+	va_list	args;
+
+	i = 0;
+	va_start(args, str);
+	while (*str)
+	{
+		if (*str == '%')
+		{
+			str++;
+			i = ft_params(*str, i, args);
+		}
+		else
+		{
+			i += write(1, str, 1);
+		}
+		str++;
+		va_end(args);
+	}
+	return (i);
+}
